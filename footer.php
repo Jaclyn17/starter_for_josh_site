b01b67a1cf0e5e4d6a2f62dae8e5b31311a2cfcc
Jaclyn17 <48287443+Jaclyn17@users.noreply.github.com>
2021-03-11 15:26:17 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+<p> <?php bloginfo('name'); ?></p>
+                <img src="http://206.189.45.97/~mesh6/wp-content/uploads/2021/03/tumbler.jpg" alt=“tumblr” >
+                <img src="http://206.189.45.97/~mesh6/wp-content/uploads/2021/03/rss.jpg" alt=“rss” >
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh6/wp-content/uploads/2021/03/linkedin.jpg" alt=“linkedin” >
+                <img src="http://206.189.45.97/~mesh6/wp-content/uploads/2021/03/fb.jpg" alt=“facebook” >
+            </div>
+
+        </div>
+    </div>
+</footer>
